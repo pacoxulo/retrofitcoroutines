d92adff920158698d3415ae41319ca92a1cb8f0f
pacoxulo <47174787+pacoxulo@users.noreply.github.com>
2019-02-28 21:17:32 +0100

    Update MainActivity.kt

--- app/src/main/java/net/azarquiel/retrofitcoroutines/MainActivity.kt
+++ app/src/main/java/net/azarquiel/retrofitcoroutines/MainActivity.kt
@@ -5,11 +5,12 @@ import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
-import net.azarquiel.recetasclase.viewmodel.MainViewModel
 import net.azarquiel.retrofitcoroutines.adapter.CustomAdapter
 import net.azarquiel.retrofitcoroutines.model.Bar
+import net.azarquiel.retrofitcoroutines.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var adapter: CustomAdapter
@@ -28,10 +29,11 @@ class MainActivity : AppCompatActivity() {
         }
 
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        viewModel.getDataBares().observe(this, Observer {
-            //adapter.setBares(it!!) // with nullable
-            //it?.let{adapter.setBares(it)} // unwrap nullable it
-            it?.let(adapter::setBares)  // to lambda
+        viewModel.loadData().observe(this, Observer { it ->
+            it?.let{
+                bares=it
+                adapter.setBares(bares)
+            }
         })
     }
 
@@ -42,12 +44,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun addBar() {
-        viewModel.saveBar("111 Paco", "micassa", "Olias", "Toledo")
+        viewModel.saveBar("000 Pepe", "micassa", "Olias", "Toledo").observe(this, Observer {
+            it?.let {
+                Toast.makeText(this,"Saved Bar...",Toast.LENGTH_LONG).show()
+                val bares = ArrayList(bares)
+                bares.add(0, it)
+                this.bares = bares
+                adapter.setBares(this.bares)
+            }
+        })
     }
-
-//    private fun uiBares(bares: List<Bar>) {
-//        for (bar in bares){
-//                Log.d("Paco", bar.toString())
-//        }
-//    }
 }
